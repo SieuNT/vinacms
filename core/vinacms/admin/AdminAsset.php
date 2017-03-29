3300b20ea01edb73f886f41d9c36b4043978911b
Nguyen Tuan Sieu <dev.phpjs@gmail.com>
2017-03-29 18:09:28 +0700

    .

---
+++ core/vinacms/admin/AdminAsset.php
@@ -0,0 +1,30 @@
+<?php
+namespace vinacms\admin;
+
+use yii\web\AssetBundle;
+/**
+ * @author Nguyen Tuan Sieu <tuan@sieulog.com>
+ * @since 1.0
+ */
+class AdminAsset extends AssetBundle
+{
+    public $sourcePath = __DIR__.'/assets';
+    public $js = [
+        'js/pace.min.js',
+        'js/sweetalert.min.js',
+        'js/jquery.nicescroll.js',
+        'js/jquery.scrollTo.min.js',
+        'js/jquery.app.js',
+    ];
+    public $css = [
+        '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300',
+        'css/bootstrap-reset.css',
+        'css/animate.css',
+        'css/font-awesome.css',
+        'css/ionicons.css',
+        'css/sweetalert.css',
+        'css/style.css',
+        'css/helper.css',
+    ];
+    public $depends = [];
+}
